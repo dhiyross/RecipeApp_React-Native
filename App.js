6fecf47ce869f3cee9bd54cefa1ab8b9c66ee898
Rossy <d.hiyross@gmail.com>
2024-09-03 10:33:35 +0700

    Add account screen

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
 
 function HomeScreen() {
+  const navigation = useNavigation();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
+      <Text>Go To Detail Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+// function DetailScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Detail Screen</Text>
+//     </View>
+//   );
+// }
+
+function DetailScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <TouchableOpacity onPress={() => navigation.navigate('Account')}>
+      <Text>Go To Account Screen</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Account Screen</Text>
     </View>
   );
 }
@@ -20,6 +51,8 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+        <Stack.Screen name="Account" component={AccountScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
